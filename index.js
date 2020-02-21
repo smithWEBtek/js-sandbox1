3a1b574b60c7ed8f23993e2c9527094ebc0386d3
Brad Smith <brad@smithwebtek.com>
2020-02-21 07:58:23 -0500

    Dusting off js

---
+++ index.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMcontentLoaded", () => {
+  let mainDiv = document.getElementById("main")
+  // mainDiv.innerHTML = '<h1>Cat pic</h1>'
+  console.log('hey this worked')
+})
