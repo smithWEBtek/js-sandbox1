0ac42c1e23193db4567e86f87c1ab48d7ca56d26
Brad Smith <brad@smithwebtek.com>
2020-07-17 09:18:14 -0400

    Add mainDiv back

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMcontentLoaded", () => {
   let mainDiv = document.getElementById("main")
-  // mainDiv.innerHTML = '<h1>Cat pic</h1>'
+  mainDiv.innerHTML = '<h1>Cat pic</h1>'
   console.log('hey this worked')
 })
